453cc202620fcc3af512e03e6bb924df4b39fe21
MercierNicolas <mr.mercier.nicolas@gmail.com>
2021-02-10 18:38:29 +0100

    Code du jeux plus ou moins fini

--- js/plusoumoins.js
+++ js/plusoumoins.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+	$("#btn").click(function(){
+		$("#nb_dit").add("#jeux > p").add("#btn_val").toggle();
+		$("#btn").toggle();
+		$("#rep").add("#tenta").text("");
+		var cible = Math.floor(Math.random() * 1000) + 1;
+		var nombre;
+		var compteur =0;
+		console.log(cible);
+		
+			$("#btn_val").click(function(){
+				nombre = $("#nb_dit").val();
+				console.log(nombre);
+
+			 if (nombre == cible){
+			 	$("#rep").text("Bravo trouver en :"+ compteur);
+			 	$("#nb_dit").add("#jeux > p").add("#btn_val").toggle();
+				$("#btn").toggle();	
+			 };
+			 if(nombre > cible){
+        		$("#rep").text("C'est moins");
+        		compteur++;	
+        		$("#tenta").text(compteur);
+        	
+   			};
+   			if(nombre < cible){
+        		$("#rep").text("C'est plus");	
+        		compteur++;	
+        		$("#tenta").text(compteur);
+  			};
+			});
 
 
 
@@ -6,5 +36,7 @@ $(document).ready(function(){
 
 
 
+
+	});
 
 });
